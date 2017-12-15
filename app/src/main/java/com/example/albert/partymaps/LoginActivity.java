f3aa0db5d3574c789690b3dc00cbcc458ae31b09
llanas <llanasnas@gmail.com>
2017-12-15 17:16:37 +0100

    loginActivity.java acctualitzat

--- app/src/main/java/com/example/albert/partymaps/LoginActivity.java
+++ app/src/main/java/com/example/albert/partymaps/LoginActivity.java
@@ -1,13 +1,40 @@
 package com.example.albert.partymaps;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        TextView logo = (TextView) findViewById(R.id.logo);
+        Typeface font = Typeface.createFromAsset(getAssets(), "fonts/RubikMonoOne-Regular.ttf");
+        logo.setTypeface(font);
+        EditText user = (EditText) findViewById(R.id.user);
+        EditText password = (EditText) findViewById(R.id.password);
+        Button submit = (Button) findViewById(R.id.submit);
+        TextView createAccount = (TextView) findViewById(R.id.create);
+        submit.setOnClickListener(this);
+
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        EditText user = (EditText) view.findViewById(R.id.user);
+        EditText password = (EditText) view.findViewById(R.id.password);
+        String contrasenya = password.getText().toString();
+        String usuari = user.getText().toString();
     }
 }
+
