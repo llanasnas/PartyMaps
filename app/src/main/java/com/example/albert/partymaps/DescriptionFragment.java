e48659ef1ad203aaf53eb1841bdf9f5d8d717e47
gerard <llanasnas@gmail.com>
2018-04-16 12:14:57 +0200

    favoritos firebase structure

--- app/src/main/java/com/example/albert/partymaps/DescriptionFragment.java
+++ app/src/main/java/com/example/albert/partymaps/DescriptionFragment.java
@@ -5,10 +5,14 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -26,6 +30,20 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.SetOptions;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 /**
@@ -36,9 +54,12 @@ public class DescriptionFragment extends Fragment  {
 
     private static GoogleMap mMap;
     private static LatLng position;
+    private static final String TAG = CrearEvento.class.getSimpleName();
+    private FirebaseAuth mAuth;
     private static Marker mark;
     private boolean favorite = false;
     private static MapFragment mapFragment;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     public DescriptionFragment() {
         // Required empty public constructor
@@ -55,9 +76,47 @@ public class DescriptionFragment extends Fragment  {
         if( ((AppCompatActivity)getActivity()).getSupportActionBar() != null){
             ((AppCompatActivity)getActivity()).getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
         }
-        Event event = getArguments().getParcelable("evento");
+        FloatingActionButton fab = (FloatingActionButton) view.findViewById(R.id.fab_fav);
+
+
+        final Event event = getArguments().getParcelable("evento");
         String ubicacion = event.getUbication().substring(10,event.getUbication().length()-1);
         String[] ubi = ubicacion.split(",");
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAuth = FirebaseAuth.getInstance();
+                FirebaseUser user = mAuth.getCurrentUser();
+                CollectionReference events = db.collection("Events");
+                //Query evento = events.whereEqualTo("date",event.getDate()).whereEqualTo("ubication",event.getUbication()).whereEqualTo("time",event.getTime());
+
+                String idevent = event.getDate() + event.getUbication() + event.getTime();
+                Map<String, Boolean> favoritos = new HashMap<>();
+                favoritos.put(idevent,true);
+
+
+
+                db.collection("Users").document(user.getUid()).collection("favoritos").document(idevent.replace("/","-")
+                        .replace(" ","")).set(favoritos)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Log.d(TAG, "DocumentSnapshot successfully written!");
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.w(TAG, "Error writing document", e);
+                            }
+                        });
+
+
+
+
+                Snackbar.make(view, "Añadido a tus doritos", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }});
         final LatLng latLng = new LatLng(Double.parseDouble(ubi[0]),Double.parseDouble(ubi[1]));
         if (mapFragment == null) {
             mapFragment = MapFragment.newInstance();
@@ -91,6 +150,7 @@ public class DescriptionFragment extends Fragment  {
                 }
             }
         });*/
+
 
         name.setText(event.getName());
         toolbar.setTitle(event.getName());
