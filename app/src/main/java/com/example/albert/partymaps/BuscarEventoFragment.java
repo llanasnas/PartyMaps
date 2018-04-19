bb843cefe15d68b685a8e2e3ea1c51d3039bdd51
morellix16 <mark.morelli16@gmail.com>
2018-04-19 21:35:59 +0200

    Permisos de GPS

--- app/src/main/java/com/example/albert/partymaps/BuscarEventoFragment.java
+++ app/src/main/java/com/example/albert/partymaps/BuscarEventoFragment.java
@@ -1,9 +1,21 @@
 package com.example.albert.partymaps;
 
 
+import android.app.AlertDialog;
 import android.app.FragmentManager;
+import android.content.ComponentName;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.support.v7.widget.AppCompatButton;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,21 +24,30 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.Spinner;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.instantapps.ActivityCompat;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import java.util.ArrayList;
+
+import static com.facebook.FacebookSdk.getApplicationContext;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class BuscarEventoFragment extends Fragment {
+public class BuscarEventoFragment extends Fragment  {
 
     private ArrayList<Event> events = new ArrayList<Event>();
     private ArrayList<Event> eventos = new ArrayList<Event>();
     private static final String TAG = RegisterActivity.class.getSimpleName();
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private FusedLocationProviderClient mFusedLocationClient;
 
 
     public BuscarEventoFragment() {
@@ -34,6 +55,7 @@ public class BuscarEventoFragment extends Fragment {
     }
 
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -44,6 +66,7 @@ public class BuscarEventoFragment extends Fragment {
         final Spinner localities = (Spinner) view.findViewById(R.id.buscar_por_localidad);
         final Spinner musicTypes = (Spinner) view.findViewById(R.id.estilo_de_musica);
         SeekBar kilometros = view.findViewById(R.id.kilometros);
+        Switch activarGPS = view.findViewById(R.id.buscar_distancia);
         final TextView numeroKilometros = view.findViewById(R.id.numero_kilometros);
         String[] arraySpinner = getResources().getStringArray(R.array.cities);
         ArrayAdapter<String> adapterLocalities = new ArrayAdapter(getActivity(), android.R.layout.simple_spinner_dropdown_item,
@@ -54,6 +77,16 @@ public class BuscarEventoFragment extends Fragment {
                 arraymusic);
         events = getActivity().getIntent().getParcelableArrayListExtra("events");
         musicTypes.setAdapter(adapterMusica);
+
+
+        activarGPS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().requestPermissions(new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            }
+        });
+
+
 
         kilometros.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
@@ -71,6 +104,8 @@ public class BuscarEventoFragment extends Fragment {
 
             }
         });
+
+
 
         AppCompatButton buscar = (AppCompatButton) view.findViewById(R.id.buscar_boton);
 
@@ -137,5 +172,7 @@ public class BuscarEventoFragment extends Fragment {
         return view;
 
     }
+
+
 
 }
