538a0fcadee99550253328444554bb7e5d4b2df4
morellix16 <mark.morelli16@gmail.com>
2018-04-22 23:41:18 +0200

    Implementado buscar por distancia, funcionando correctamente (queda por arreglar la busqueda de distancia por menos de 2 digitos)

--- app/src/main/java/com/example/albert/partymaps/BuscarEventoActivity.java
+++ app/src/main/java/com/example/albert/partymaps/BuscarEventoActivity.java
@@ -1,13 +1,41 @@
 package com.example.albert.partymaps;
 
+import android.*;
+import android.accounts.Account;
+import android.accounts.AccountManager;
 import android.app.FragmentManager;
 import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.net.Uri;
 import android.os.Bundle;
+import android.Manifest;
 
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QuerySnapshot;
@@ -15,12 +43,20 @@ import com.google.firebase.firestore.QuerySnapshot;
 import java.util.ArrayList;
 import java.util.List;
 
-public class BuscarEventoActivity extends AppCompatActivity {
+public class BuscarEventoActivity extends AppCompatActivity  {
 
     private ArrayList<Event> events = new ArrayList<Event>();
     private ArrayList<Event> eventos = new ArrayList<Event>();
     private static final String TAG = RegisterActivity.class.getSimpleName();
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private FusedLocationProviderClient mFusedLocationClient;
+    private LocationRequest mLocationRequest;
+    private GoogleApiClient client;
+    private PendingResult<LocationSettingsResult> result;
+    private static final Integer GPS_SETTINGS = 0x7;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,16 +64,74 @@ public class BuscarEventoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_buscar_evento);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        client = new GoogleApiClient.Builder(this)
+                .addApi(AppIndex.API)
+                .addApi(LocationServices.API)
+                .build();
         BuscarEventoFragment buscarEventoFragment = new BuscarEventoFragment();
         FragmentManager fragmentManager = getFragmentManager();
         fragmentManager.beginTransaction().
                 replace(R.id.buscar_eventos, buscarEventoFragment).
                 commit();
 
-
-
-
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (ActivityCompat.checkSelfPermission(this, permissions[0]) == PackageManager.PERMISSION_GRANTED) {
+            switch (requestCode) {
+                //Location
+                case 1:
+                    askForGPS();
+                    break;
+                //Call
+
+            }
+        }
+    }
+
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        client.connect();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        client.disconnect();
+    }
+
+    private void askForGPS(){
+        mLocationRequest = LocationRequest.create();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder().addLocationRequest(mLocationRequest);
+        builder.setAlwaysShow(true);
+        result = LocationServices.SettingsApi.checkLocationSettings(client, builder.build());
+        result.setResultCallback(new ResultCallback<LocationSettingsResult>() {
+            @Override
+            public void onResult(LocationSettingsResult result) {
+                final Status status = result.getStatus();
+                switch (status.getStatusCode()) {
+                    case LocationSettingsStatusCodes.SUCCESS:
+                        break;
+                    case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                        try {
+                            status.startResolutionForResult(BuscarEventoActivity.this, GPS_SETTINGS);
+                        } catch (IntentSender.SendIntentException e) {
+
+                        }
+                        break;
+                    case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                        break;
+                }
+            }
+        });
+    }
+
 
 }
--- app/src/main/java/com/example/albert/partymaps/BuscarEventoFragment.java
+++ app/src/main/java/com/example/albert/partymaps/BuscarEventoFragment.java
@@ -1,11 +1,14 @@
 package com.example.albert.partymaps;
 
 
+import android.*;
+import android.Manifest;
 import android.app.AlertDialog;
 import android.app.FragmentManager;
 import android.content.ComponentName;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
@@ -15,7 +18,9 @@ import android.os.Bundle;
 import android.app.Fragment;
 import android.provider.Settings;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.AppCompatButton;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -28,9 +33,18 @@ import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.instantapps.ActivityCompat;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import java.util.ArrayList;
@@ -48,14 +62,28 @@ public class BuscarEventoFragment extends Fragment  {
     private static final String TAG = RegisterActivity.class.getSimpleName();
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     private FusedLocationProviderClient mFusedLocationClient;
+    GoogleApiClient client;
+    LocationRequest mLocationRequest;
+    PendingResult<LocationSettingsResult> result;
+    static final Integer GPS_SETTINGS = 0x7;
+    static final Integer LOCATION = 0x1;
+
 
 
     public BuscarEventoFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        client = new GoogleApiClient.Builder(getActivity())
+                .addApi(AppIndex.API)
+                .addApi(LocationServices.API)
+                .build();
 
-    @RequiresApi(api = Build.VERSION_CODES.M)
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -65,8 +93,8 @@ public class BuscarEventoFragment extends Fragment  {
         final EditText nombre = (EditText) view.findViewById(R.id.bpn);
         final Spinner localities = (Spinner) view.findViewById(R.id.buscar_por_localidad);
         final Spinner musicTypes = (Spinner) view.findViewById(R.id.estilo_de_musica);
-        SeekBar kilometros = view.findViewById(R.id.kilometros);
-        Switch activarGPS = view.findViewById(R.id.buscar_distancia);
+        final SeekBar kilometros = view.findViewById(R.id.kilometros);
+        final Switch activarGPS = view.findViewById(R.id.buscar_distancia);
         final TextView numeroKilometros = view.findViewById(R.id.numero_kilometros);
         String[] arraySpinner = getResources().getStringArray(R.array.cities);
         ArrayAdapter<String> adapterLocalities = new ArrayAdapter(getActivity(), android.R.layout.simple_spinner_dropdown_item,
@@ -82,7 +110,7 @@ public class BuscarEventoFragment extends Fragment  {
         activarGPS.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                getActivity().requestPermissions(new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+                askForPermission(Manifest.permission.ACCESS_FINE_LOCATION,LOCATION);
             }
         });
 
@@ -113,9 +141,9 @@ public class BuscarEventoFragment extends Fragment  {
             @Override
             public void onClick(View v) {
 
-                if (nombre.getText().toString().isEmpty() && musicTypes.getSelectedItem().toString().equals("Estilo musical") && localities.getSelectedItem().toString().equals("Localidad")) {
+                if (nombre.getText().toString().isEmpty() && musicTypes.getSelectedItem().toString().equals("Estilo musical") && localities.getSelectedItem().toString().equals("Localidad") && !activarGPS.isChecked()) {
 
-                    nombre.setError("Busca por nombre, localidad o estilo musical");
+                    nombre.setError("Busca por nombre, localidad, estilo musical o distancia");
 
                 } else {
 
@@ -152,6 +180,33 @@ public class BuscarEventoFragment extends Fragment  {
                                     break;
                                 }
                             }
+
+                            if (activarGPS.isChecked()){
+                                GPSTracker gpsTracker = new GPSTracker(getApplicationContext());
+                                Float distanciaMaxima = Float.parseFloat(numeroKilometros.getText().toString().substring(numeroKilometros.getText().toString().length() - 2))*1000;
+                                if (gpsTracker.getLocation() != null){
+                                    Location locationUser = gpsTracker.getLocation();
+                                    Location locationEvent = new Location("evento");
+                                    String ubicacionEvento = event.getUbication();
+
+                                    ubicacionEvento = ubicacionEvento.replaceAll("lat/lng:","");
+
+                                    ubicacionEvento = ubicacionEvento.substring(2,ubicacionEvento.length()-1);
+
+                                    String[] latLong = ubicacionEvento.split(",");
+
+                                    locationEvent.setLatitude(Float.parseFloat(latLong[0]));
+
+                                    locationEvent.setLongitude(Float.parseFloat(latLong[1]));
+
+                                    Float distancia = locationUser.distanceTo(locationEvent);
+
+                                    if (distancia <= distanciaMaxima){
+                                        eventos.add(event);
+                                        break;
+                                    }
+                                }
+                            }
                             added = true;
                         }
                     }
@@ -170,9 +225,24 @@ public class BuscarEventoFragment extends Fragment  {
             }
         });
         return view;
-
     }
 
+    private void askForPermission(String permission, Integer requestCode) {
+        if (ContextCompat.checkSelfPermission(getActivity(), permission) != PackageManager.PERMISSION_GRANTED) {
 
+            // Should we show an explanation?
+            if (android.support.v4.app.ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), permission)) {
 
+                //This is called if user has denied the permission before
+                //In this case I am just asking the permission again
+                android.support.v4.app.ActivityCompat.requestPermissions(getActivity(), new String[]{permission}, requestCode);
+
+            } else {
+
+                android.support.v4.app.ActivityCompat.requestPermissions(getActivity(), new String[]{permission}, requestCode);
+            }
+        } else {
+            Toast.makeText(getActivity(), "" + permission + " is already granted.", Toast.LENGTH_SHORT).show();
+        }
+    }
 }
